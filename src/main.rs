90e50ca8f0e0eb7b1b986c89e13201301b7317de
Matt Jordan <mattj@allenai.org>
2025-02-26 21:13:59 -0800

    Added reshard and id-adder

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,19 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::path::PathBuf;
 use std::time::Instant;
+use std::cmp::max;
+use std::sync::atomic::{AtomicUsize, Ordering};
 
 use serde_json;
 use serde_yaml;
-use anyhow::{Error, Result};
+use anyhow::{Error, Result, ensure};
 use clap::{Parser, Subcommand};
 use rayon::prelude::*;
+use rayon::current_num_threads;
 use mj_io::{expand_dirs, read_pathbuf_to_mem, write_mem_to_pathbuf, build_pbar};
 use rand::Rng;
 use phf::phf_map;
-
+use uuid::Uuid;
 
 
 /*============================================================
@@ -45,6 +48,20 @@ enum Commands {
 
         #[arg(required=true, long)]
         config: PathBuf
+    },
+
+    Reshard {
+        #[arg(required=true, long)]
+        input_dir: PathBuf,
+
+        #[arg(required=true, long)]
+        output_dir: PathBuf,
+
+        #[arg(long, default_value_t=0)]
+        max_lines: usize,
+
+        #[arg(long, default_value_t=0)]
+        max_size: usize,
     }
 
 
@@ -96,6 +113,7 @@ fn parse_config(config: &PathBuf) -> Result<serde_json::Value, Error> {
 static MAP_FXNS : phf::Map<&'static str, fn(String, &serde_json::Value) -> Result<String, Error>> = phf_map! {
     "subsample" => subsample_line,
     "len_filter" => len_filter_line,
+    "add_id" => add_id_line
 };
 
 
@@ -195,6 +213,98 @@ fn len_filter_line(line: String, config: &serde_json::Value) -> Result<String, E
 }
 
 
+/*============================================================
+=                            ADD ID                          =
+============================================================*/
+
+fn add_id_line(line: String, config: &serde_json::Value) -> Result<String, Error> {
+    let mut json_obj : serde_json::Value = serde_json::from_str(&line).unwrap();
+    let id_key = match config.get("id_key") {
+        Some(id_key) => id_key.to_string(),
+        None => "id".to_string()
+    };
+
+    json_obj[id_key] = serde_json::Value::String(Uuid::new_v4().to_string());
+
+    let new_line = json_obj.to_string();
+    Ok(new_line)
+}
+
+
+
+/*============================================================
+=                            RESHARD                         =
+============================================================*/
+
+fn reshard(input_dir: &PathBuf, output_dir: &PathBuf, max_lines: usize, max_size: usize) -> Result<(), Error> {
+    let start_main = Instant::now();
+
+    ensure!(max(max_lines, max_size) > 0, "Either max_lines or max_size must be provided!");
+    let max_lines = if max_lines == 0 {
+        usize::MAX
+    } else {
+        max_lines
+    };
+    let max_size = if max_size == 0 {
+        usize::MAX
+    } else {
+        max_size
+    };
+
+
+    let num_threads = current_num_threads();    
+    let all_files = expand_dirs(vec![input_dir.clone()], None).unwrap();
+    let chunk_size = (all_files.len() + num_threads - 1) / num_threads;
+    let chunks: Vec<Vec<PathBuf>> = all_files.chunks(chunk_size).map(|c| c.to_vec()).collect();
+    let out_num = AtomicUsize::new(0);
+    chunks.par_iter().for_each(|chunk| {
+        reshard_chunk(chunk, output_dir, &out_num, max_lines, max_size).unwrap();
+    });
+
+    println!("Finished reshard in {:?} seconds | Wrote {:?} new shards", start_main.elapsed().as_secs(), out_num.fetch_add(0, Ordering::SeqCst));
+    Ok(())
+}
+
+
+fn reshard_chunk(chunk: &Vec<PathBuf>, output_dir: &PathBuf, out_num: &AtomicUsize, max_lines: usize, max_size: usize) -> Result<(), Error> {
+
+    let mut cur_data: Vec<u8> = Vec::new();
+    let mut cur_lines = 0;
+    let mut cur_size = 0;
+    for path in chunk {
+        let data = read_pathbuf_to_mem(path).unwrap();
+        for line in data.lines() {
+            let line = line.unwrap();
+            let line = line.as_bytes();
+            cur_lines += 1;
+            cur_size += line.len();
+            cur_data.extend(line);
+            cur_data.push(b'\n');
+            if cur_lines >= max_lines || cur_size >= max_size {
+                write_new_shard(cur_data, output_dir, out_num).unwrap();
+                cur_data = Vec::new();
+                cur_lines = 0;
+                cur_size = 0;
+            }
+        }
+    }
+    if cur_data.len() > 0 {
+        write_new_shard(cur_data, output_dir, out_num).unwrap();
+    }
+
+    Ok(())
+}
+
+fn write_new_shard(data: Vec<u8>, output_dir: &PathBuf, out_num: &AtomicUsize) -> Result<(), Error> {
+    let shard_id = out_num.fetch_add(1, Ordering::SeqCst);
+    let basename = PathBuf::from(format!("shard_{:08}.jsonl.zst", shard_id));
+    let output_file = output_dir.clone().join(basename);
+
+    write_mem_to_pathbuf(&data, &output_file).unwrap();
+    Ok(())
+}
+
+
 
 /*============================================================
 =                            MAIN                            =
@@ -210,6 +320,9 @@ fn main() {
     let result = match &args.command {
         Commands::Map{input_dir, output_dir, config} => {
             gen_map(input_dir, output_dir, config)
+        },
+        Commands::Reshard{input_dir, output_dir, max_lines, max_size} => {
+            reshard(input_dir, output_dir, *max_lines, *max_size)
         }
         _ => {Ok(())}
     };
