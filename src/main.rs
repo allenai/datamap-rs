5fa0dc24de1e4c66915187efa4601e9f4663479a
Matt Jordan <mattj@allenai.org>
2025-03-10 17:15:03 -0700

    Added some cleaner form of mapping

--- src/main.rs
+++ src/main.rs
@@ -9,17 +9,16 @@ use std::cmp::max;
 use std::sync::atomic::{AtomicUsize, Ordering};
 
 use serde_json;
-use serde_json::json;
 use serde_yaml;
 use anyhow::{Error, Result, ensure};
 use clap::{Parser, Subcommand};
 use rayon::prelude::*;
 use rayon::current_num_threads;
 use mj_io::{expand_dirs, read_pathbuf_to_mem, write_mem_to_pathbuf, build_pbar};
-use rand::Rng;
-use phf::phf_map;
-use uuid::Uuid;
 use indicatif::ProgressBar;
+
+mod map_fxn; 
+use crate::map_fxn::{CompiledProcessor, precompile_processor};
 /*
 Map Config layout:
 
@@ -59,16 +58,6 @@ enum Commands {
         config: PathBuf
     },
 
-    Tag {
-        #[arg(required=true, long)]
-        input_dir: PathBuf,
-
-        #[arg(required=true, long)]
-        output_dir: PathBuf,
-
-        #[arg(required=true, long)]
-        config: PathBuf
-    },
 
     Reshard {
         #[arg(required=true, long)]
@@ -83,17 +72,6 @@ enum Commands {
         #[arg(long, default_value_t=0)]
         max_size: usize,
     },
-
-    Stats {
-        #[arg(required=true, long)]
-        input_dir: PathBuf,
-
-        #[arg(required=true, long)]
-        output_file: PathBuf,
-
-        #[arg(required=true, long)]
-        config: PathBuf
-    }
 
 }
 
@@ -139,25 +117,24 @@ fn parse_config(config: &PathBuf) -> Result<serde_json::Value, Error> {
 =                            GENERAL MAP                     =
 ============================================================*/
 
-static MAP_FXNS : phf::Map<&'static str, fn(String, &serde_json::Value) -> Result<String, Error>> = phf_map! {
-    "subsample" => subsample_line,
-    "len_filter" => len_filter_line,
-    "add_id" => add_id_line,
-    "santacoder_pl_filter" => santacoder_pl_filter
-};
-
 
 fn gen_map(input_dir: &PathBuf, output_dir: &PathBuf, config: &PathBuf) -> Result<(), Error> {
+    /* Generic mapping/filtration function. 
+
+    Processes each *.jsonl.* in input_dir and makes an identically named copy in output_dir
+    with the changes specified in the config applied
+    */
+
     let start_main = Instant::now();
 
     let all_files = expand_dirs(vec![input_dir.clone()], None).unwrap();
     let json_config = parse_config(config).unwrap();
-    
+    let processor = precompile_processor(&json_config).unwrap();
 
     let pbar = build_pbar(all_files.len(), "Files");
     all_files.par_iter().for_each(|p| {
-        let output_file = get_output_file_name(input_dir, output_dir, p).unwrap();
-        gen_map_single(p, &output_file, &json_config).unwrap();
+        let output_file = get_output_file_name(input_dir, output_dir, p).unwrap();        
+        gen_map_single(p, &output_file, &processor).unwrap();
         pbar.inc(1);
     });
 
@@ -169,313 +146,29 @@ fn gen_map(input_dir: &PathBuf, output_dir: &PathBuf, config: &PathBuf) -> Resul
 
 
 
-fn gen_map_single(input_file: &PathBuf, output_file: &PathBuf, json_config: &serde_json::Value) -> Result<(), Error> {
+fn gen_map_single(input_file: &PathBuf, output_file: &PathBuf, processor: &CompiledProcessor) -> Result<(), Error> {
+    /* Single-file mapping/filtration function
+
+    Processes the contents of a single file, using file-centric mappers specified in the config and writes to output file
+    */
     let data = read_pathbuf_to_mem(input_file).unwrap();
-    let mut output_bytes: Vec<u8> = Vec::new();
 
-
-    for line in data.lines() {
-        let line = line.unwrap();
-        let output_line = map_pipeline(line, json_config).unwrap();
-        if output_line.len() > 0 {
-            output_bytes.extend(output_line.as_bytes());
+    let lines: Vec<_> = data.lines().map(|el| el.unwrap()).collect();
+    let output_lines = processor.process(lines).unwrap();
+    if output_lines.len() > 0 {
+        let mut output_bytes: Vec<u8> = Vec::new();
+        for line in output_lines.into_iter() {
+            output_bytes.extend(line.as_bytes());
             output_bytes.push(b'\n');
         }
-    }
-
-    if output_bytes.len() > 0 {
-        write_mem_to_pathbuf(&output_bytes, output_file).unwrap();
-    }
-
-    Ok(())
-}
-
-
-
-fn map_pipeline(line: String, json_config: &serde_json::Value) -> Result<String, Error> {
-    let pipeline = json_config["pipeline"].as_array().unwrap().clone();
-
-    for map_fxn in pipeline {
-        let fxn_name = map_fxn.get("name").unwrap().as_str().unwrap();
-        let process_line = MAP_FXNS[fxn_name];
-        let line = process_line(line.clone(), &map_fxn.get("kwargs").unwrap()).unwrap();
-        if line.len() == 0 {
-            return Ok(String::new());
-        }
-    }
-    Ok(line)
-}
-
-
-
-fn subsample_line(line: String, config: &serde_json::Value) -> Result<String, Error> {
-    let mut rng = rand::rng();
-    let random_float = rng.random::<f64>();
-    let ratio = config.get("ratio").unwrap().as_f64().unwrap();
-
-    let output = if random_float <= ratio {
-        line
-    } else {
-        String::new()
-    };
-
-    Ok(output)
-}
-
-
-fn len_filter_line(line: String, config: &serde_json::Value) -> Result<String, Error> {
-    let min_len = match config.get("min_len") {
-        Some(min_len) => min_len.as_u64().unwrap() as usize,
-        None => 0
-    };
-
-    let max_len = match config.get("max_len") {
-        Some(max_len) => max_len.as_u64().unwrap() as usize,
-        None => usize::MAX
-    };
-
-    let json_obj : serde_json::Value = serde_json::from_str(&line).unwrap();
-    let textlen = json_obj.get("text").unwrap().as_str().unwrap().len();
-
-    let output = if textlen <= max_len && textlen >= min_len {
-        line
-    } else {
-        String::new()
-    };
-    Ok(output)
-
-}
-
-fn add_id_line(line: String, config: &serde_json::Value) -> Result<String, Error> {
-    let mut json_obj : serde_json::Value = serde_json::from_str(&line).unwrap();
-    let id_key = match config.get("id_key") {
-        Some(id_key) => id_key.to_string(),
-        None => "id".to_string()
-    };
-
-    json_obj[id_key] = serde_json::Value::String(Uuid::new_v4().to_string());
-
-    let new_line = json_obj.to_string();
-    Ok(new_line)
-}
-
-
-fn santacoder_pl_filter(line: String, _config: &serde_json::Value) -> Result<String, Error> {
-    let json_obj : serde_json::Value = serde_json::from_str(&line).unwrap();
-
-    let pl = json_obj.get("metadata").and_then(|m| m.get("language")).and_then(|l| l.as_str()).unwrap();
-    let output = if pl == "Python" || pl == "Java" || pl == "JavaScript"{
-        line
-    } else {
-        String::new()
-    };
-    Ok(output)
-}
-
-/*============================================================
-=                            GENERAL TAG                     =
-============================================================*/
-
-static TAG_FXNS : phf::Map<&'static str, fn(&serde_json::Value, &serde_json::Value) -> Result<serde_json::Value, Error>> = phf_map! {
-    // Put tag functions here
-};
-
-
-fn gen_tag(input_dir: &PathBuf, output_dir: &PathBuf, config: &PathBuf) -> Result<(), Error> {
-    let start_main = Instant::now();
-    let all_files = expand_dirs(vec![input_dir.clone()], None).unwrap();
-    let json_config = parse_config(config).unwrap();
-
-    let pbar = build_pbar(all_files.len(), "Files");
-    all_files.par_iter().for_each(|p| {
-        let output_file = get_output_file_name(input_dir, output_dir, p).unwrap();
-        gen_tag_single(p, &output_file, &json_config).unwrap();
-        pbar.inc(1);
-    });
-
-    println!("Finished tagging in {:?} seconds", start_main.elapsed().as_secs());
-    Ok(())
-}
-
-fn gen_tag_single(input_file: &PathBuf, output_file: &PathBuf, json_config: &serde_json::Value) -> Result<(), Error> {
-    let data = read_pathbuf_to_mem(input_file).unwrap();
-    let mut output_bytes: Vec<u8> = Vec::new();
-
-
-    for line in data.lines() {
-        let line = line.unwrap();
-        let attributes = tag_pipeline(&line, json_config).unwrap();
-        if attributes.len() > 0 {
-            output_bytes.extend(attributes.as_bytes());
-            output_bytes.push(b'\n');
-        }
-    }
-
-    if output_bytes.len() > 0 {
         write_mem_to_pathbuf(&output_bytes, output_file).unwrap();
     }
 
 
-    Ok(())
-}
-
-
-fn tag_pipeline(line: &String, json_config: &serde_json::Value) -> Result<String, Error> {
-    let data: serde_json::Value = serde_json::from_str(line.as_str()).unwrap();
-    let pipeline = json_config["pipeline"].as_array().unwrap().clone();
-
-
-    let mut attributes = serde_json::json!({});
-
-
-    let id_key = match json_config.get("id_key") {
-        Some(id_key) => id_key.to_string(),
-        None => "id".to_string()
-    };
-    attributes[id_key] = data.get(&id_key).unwrap().clone();
-    for tag_fxn in pipeline {
-        let fxn_name = tag_fxn.get("name").unwrap().as_str().unwrap();
-        let attr_name = tag_fxn.get("attr_name").unwrap().as_str().unwrap();
-
-
-        let process_line = TAG_FXNS[fxn_name];
-        let attr = process_line(&data, &tag_fxn.get("kwargs").unwrap()).unwrap();
-        attributes[attr_name] = attr;
-    
-    }
-
-    Ok(attributes.to_string())
-}
-
-/*============================================================
-=                            STATS                           =
-============================================================*/
-
-static STATS_FXNS : phf::Map<&'static str, fn(&serde_json::Value, serde_json::Value, &str, &serde_json::Value) -> Result<serde_json::Value, Error>> = phf_map! {
-    // Put stats functions here
-
-    "COUNT_EMPTY" => count_empty,
-    "COUNT_TOTAL" => count_total
-};
-
-
-fn stats(input_dir: &PathBuf, output_file: &PathBuf, config: &PathBuf) -> Result<(), Error> {
-    // General flow: make thread-wise stats dicts and then aggregate via sum at the end
-    let start_main = Instant::now();
-
-    let all_files = expand_dirs(vec![input_dir.clone()], None).unwrap();
-
-    let json_config = parse_config(config).unwrap();
-    let pbar = build_pbar(all_files.len(), "Files");
-    let num_threads = current_num_threads();
-    let chunk_size = (all_files.len() + num_threads - 1) / num_threads;
-    let chunks: Vec<Vec<PathBuf>> = all_files.chunks(chunk_size).map(|c| c.to_vec()).collect();
-
-    let chunk_outs: Vec<serde_json::Value> = chunks.par_iter().map(|chunk| {
-        stats_chunk(&chunk, &json_config, &pbar).unwrap()
-    }).collect();
-
-    let agg = stats_aggregate(chunk_outs, &json_config).unwrap();
-
-    println!("AGG {:?}", agg);
-    let agg_bytes = serde_json::to_vec(&agg).unwrap();
-    write_mem_to_pathbuf(&agg_bytes.as_slice(), output_file).unwrap();
-    println!("OUTPUT IS {:?}", agg);
-    println!("FINISHED STATS IN {:?} SECONDS", start_main.elapsed().as_secs());
 
     Ok(())
 }
 
-
-fn stats_chunk(chunk: &Vec<PathBuf>, json_config: &serde_json::Value, pbar: &ProgressBar) -> Result<serde_json::Value, Error> {
-    let mut chunk_stats = json!({});
-
-    for path in chunk {
-        let data = read_pathbuf_to_mem(path).unwrap();
-        for line in data.lines() {
-            let line = line.unwrap();
-            chunk_stats = stats_pipeline(&line, chunk_stats, json_config).unwrap();
-        }
-        pbar.inc(1);
-    }
-    Ok(chunk_stats)
-}
-
-fn stats_pipeline(line: &String, mut chunk_stats: serde_json::Value, json_config: &serde_json::Value) -> Result<serde_json::Value, Error> {
-    let pipeline = json_config["pipeline"].as_array().unwrap().clone();
-    let json_line: serde_json::Value = serde_json::from_str(line).unwrap();
-
-    for stat_fxn in pipeline {
-        let fxn_name = stat_fxn.get("name").unwrap().as_str().unwrap();
-        let process_json = STATS_FXNS[fxn_name];
-        let stat_name = stat_fxn.get("stat_name").unwrap().as_str().unwrap();
-        chunk_stats = process_json(&json_line, chunk_stats, stat_name, &stat_fxn.get("kwargs").unwrap()).unwrap();    
-    }
-
-    Ok(chunk_stats)
-}
-
-
-fn count_empty(json_line: &serde_json::Value, mut chunk_stats: serde_json::Value, stat_name: &str, kwargs: &serde_json::Value) -> Result<serde_json::Value, Error> {
-    let mut cur_val = match chunk_stats.get(stat_name) {
-        Some(serde_json::Value::Number(n)) => {
-            n.as_u64().unwrap()
-        },
-        _ => 0
-    };
-    if json_line.get(kwargs.get("text_field").unwrap().as_str().unwrap()).unwrap().as_str().unwrap().len() == 0 {
-        cur_val += 1
-    }
-
-    chunk_stats[stat_name] = json!(cur_val);
-
-    Ok(chunk_stats)
-}
-
-
-fn count_total(_json_line: &serde_json::Value, mut chunk_stats: serde_json::Value, stat_name: &str, _kwargs: &serde_json::Value) -> Result<serde_json::Value, Error> {
-    let cur_val = match chunk_stats.get(stat_name) {
-        Some(serde_json::Value::Number(n)) => {
-            n.as_u64().unwrap()
-        },
-        _ => 0
-    };
-
-    chunk_stats[stat_name] = json!(cur_val + 1);
-
-    Ok(chunk_stats)
-}
-
-
-fn stats_aggregate(chunk_stats: Vec<serde_json::Value>, json_config: &serde_json::Value) -> Result<serde_json::Value, Error> {
-
-    // Pipelines have aggregators
-    let mut agg = json!({});
-    let pipeline = json_config["pipeline"].as_array().unwrap().clone();
-
-    for chunk_stat in chunk_stats {
-        for el in pipeline.iter() {
-            let agg_fxn = el.get("agg_fxn").unwrap().as_str().unwrap();
-            let stat_name = el.get("stat_name").unwrap().as_str().unwrap();
-            match agg_fxn {
-                "sum" => {
-                    let chunk_value = chunk_stat.get(stat_name).unwrap().as_u64().unwrap();
-        
-                    let cur_agg_value: u64 = match agg.get(stat_name) {
-                        Some(serde_json::Value::Number(n)) => n.as_u64().unwrap(),
-                        _ => 0
-                    };
-                    agg[stat_name] = (cur_agg_value + chunk_value).into();
-                },
-                _ => return Err(Error::msg("Only sum stats supported for now!"))
-            }
-
-        }
-    }
-
-    Ok(agg)
-
-}
 
 
 /*============================================================
@@ -570,17 +263,12 @@ fn main() {
             gen_map(input_dir, output_dir, config)
         },
 
-        Commands::Tag{input_dir, output_dir, config} => {
-            gen_tag(input_dir, output_dir, config)
-        },
 
         Commands::Reshard{input_dir, output_dir, max_lines, max_size} => {
             reshard(input_dir, output_dir, *max_lines, *max_size)
         },
 
-        Commands::Stats{input_dir, output_file, config} => {
-            stats(input_dir, output_file, config)
-        }
+
         _ => {Ok(())}
     };
     result.unwrap();
---
+++ src/map_fxn.rs
@@ -0,0 +1,204 @@
+/*
+
+Library for specific map/filter functions:
+
+The general signature of these should look like:
+
+fn mapper(Vec<String>, serde_json::Value) -> Result<Vec<String>, Error>
+
+where each element of the Vec<String> is a line of the jsonl object.
+
+*/
+
+use serde_json;
+use serde_json::json;
+use anyhow::{Error, Result};
+use rand::Rng;
+use phf::phf_map;
+use uuid::Uuid;
+
+
+pub static MAP_LINE_FXNS : phf::Map<&'static str, fn(String, &serde_json::Value) -> Result<String, Error>> = phf_map! {
+	"SUBSAMPLE" => subsample_line,
+};
+
+
+pub static MAP_JSON_FXNS : phf::Map<&'static str, fn(serde_json::Value, &serde_json::Value) -> Result<Option<serde_json::Value>, Error>> = phf_map! {
+	"LEN_FILTER" => len_filter_json,
+	"ADD_ID" => add_id_json,
+	"SANTACODER_PL_FILTER" => santacoder_pl_filter_json,
+};
+
+pub static MAP_JSONL_FXNS: phf::Map<&'static str, fn(Vec<String>, &serde_json::Value) -> Result<Vec<String>, Error>> = phf_map! {
+
+};
+
+
+
+/*===========================================================================
+=                            GENERIC HELPERS                                =
+===========================================================================*/
+
+
+pub struct CompiledProcessor {
+	groups: Vec<Vec<serde_json::Value>>,
+}
+
+impl CompiledProcessor {
+	pub fn process(&self, mut lines: Vec<String>) -> Result<Vec<String>, Error> {
+	    for group in self.groups.iter() {
+	    	let group_type = group[0]["type"].as_str().unwrap();
+	    	if group_type == "WHOLE_DOC" {
+		        let fxn = MAP_JSONL_FXNS.get(&group[1]["name"].as_str().unwrap()).unwrap();
+		        lines = fxn(lines, &group[1]).unwrap();
+		    } else if group_type == "JSON_LINES" {
+		    	let mut jsons: Vec<serde_json::Value> = lines.into_iter().map(|line| serde_json::from_str(&line).unwrap()).collect();
+		    	for group_el in group[1..].iter() {
+		    		let json_map_fxn = MAP_JSON_FXNS.get(&group_el.as_str().unwrap()).unwrap();
+		    		jsons = jsons
+		    			.into_iter()
+		    			.filter_map(|j| {
+		    				json_map_fxn(j, &group_el).unwrap()
+		    			})
+		    			.collect();
+		    	}		    	
+		    	lines = jsons.into_iter().map(|j| j.to_string()).collect();
+		    } else if group_type == "RAW_LINES" {
+		    	for group_el in group[1..].iter() {
+		    		let line_map_fxn = MAP_LINE_FXNS.get(&group_el["name"].as_str().unwrap()).unwrap();
+		    		lines = lines
+		    			.into_iter()
+		    			.map(|line| line_map_fxn(line, &group_el).unwrap())
+		    			.filter(|l| l.len() > 0)
+		    			.collect();
+		    	}
+		    }
+	    }
+	    Ok(lines)
+	}
+}
+
+
+
+pub fn precompile_processor(json_config: &serde_json::Value) -> Result<CompiledProcessor, Error> {
+	// First make groups 
+
+	let mut groups: Vec<Vec<serde_json::Value>> = Vec::new();
+	let mut cur: Vec<serde_json::Value> = Vec::new();
+	let cur_group_type = String::new();
+	let pipeline = json_config["pipeline"].as_array().unwrap().clone();
+
+	for map_fxn in pipeline {
+		let name = map_fxn["name"].as_str().unwrap();
+		let group_type = if MAP_JSONL_FXNS.contains_key(name) {
+			"WHOLE_DOC"
+		} else if MAP_LINE_FXNS.contains_key(name) {
+			"RAW_LINES"
+		} else {
+			"JSON_LINES"
+		};
+
+		if group_type == cur_group_type {
+			cur.push(map_fxn)
+		} else if cur.len() == 0 {
+			cur = Vec::new();
+			cur.push(json!({"type": group_type}));
+			cur.push(map_fxn);
+		} else {
+			groups.push(cur);
+			cur = Vec::new();
+			cur.push(json!({"type": group_type}));
+			cur.push(map_fxn);			
+		}
+
+	}
+
+	if cur.len() > 0 {
+		groups.push(cur);
+	}
+
+
+	let processor = CompiledProcessor { groups };
+	Ok(processor)
+
+}
+
+
+
+
+/*================================================================================
+=                            WHOLE DOC MAPPERS                                   =
+================================================================================*/
+
+
+/*================================================================================
+=                            RAW LINE MAPPERS                                    =
+================================================================================*/
+
+
+fn subsample_line(line: String, config_json: &serde_json::Value) -> Result<String, Error> {
+	let mut rng = rand::rng();
+	let ratio = config_json.get("ratio").unwrap().as_f64().unwrap();
+	let random_float = rng.random::<f64>();
+	if random_float <= ratio {
+		Ok(line)
+	} else {
+		Ok(String::new())
+	}
+}
+
+
+
+/*================================================================================
+=                            JSON MAPPERS                                        =
+================================================================================*/
+
+
+fn len_filter_json(json_obj: serde_json::Value, config_json: &serde_json::Value) -> Result<Option<serde_json::Value>, Error> {
+    let min_len = match config_json.get("min_len") {
+        Some(min_len) => min_len.as_u64().unwrap() as usize,
+        None => 0
+    };
+
+    let max_len = match config_json.get("max_len") {
+        Some(max_len) => max_len.as_u64().unwrap() as usize,
+        None => usize::MAX
+    };    
+    let textlen = json_obj.get("text").unwrap().as_str().unwrap().len();
+
+    if textlen <= max_len && textlen >= min_len {
+    	Ok(Some(json_obj))
+    } else {
+    	Ok(None)
+    }
+}
+
+
+
+fn add_id_json(mut json_obj: serde_json::Value, config_json: &serde_json::Value) -> Result<Option<serde_json::Value>, Error> {
+    let id_key = match config_json.get("id_key") {
+        Some(id_key) => id_key.to_string(),
+        None => "id".to_string()
+    };
+    json_obj[&id_key] = serde_json::Value::String(Uuid::new_v4().to_string());
+    Ok(Some(json_obj))
+
+}
+
+
+fn santacoder_pl_filter_json(json_obj: serde_json::Value, _config_json: &serde_json::Value) -> Result<Option<serde_json::Value>, Error> {
+    let pl = json_obj.get("metadata").and_then(|m| m.get("language")).and_then(|l| l.as_str()).unwrap();
+    if pl == "Python" || pl == "Java" || pl == "JavaScript"{
+    	Ok(Some(json_obj))
+    } else {
+    	Ok(None)
+    }
+}
+
+
+
+
+
+
+
+
