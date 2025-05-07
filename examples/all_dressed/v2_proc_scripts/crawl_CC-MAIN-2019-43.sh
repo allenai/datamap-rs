ef41082e58f2557513f045392336fac61c9b28fc
Matt Jordan <mattj@allenai.org>
2025-05-07 11:14:27 -0700

    Updated proc scripts to check existence

--- examples/all_dressed/v2_proc_scripts/crawl_CC-MAIN-2019-43.sh
+++ examples/all_dressed/v2_proc_scripts/crawl_CC-MAIN-2019-43.sh
@@ -3,14 +3,29 @@
 
 
 # Store the input argument
-X=crawl=CC-MAIN-2019-43
+X=crawl_CC-MAIN-2019-43
+
+
 echo "Processing directory: $X"
+
+# Step -1:
+TARGET_DIR="s3://ai2-llm/pretraining-data/sources/cc_all_dressed/all_dressed_v2/english/$X/"
+FILE_COUNT=$(aws s3 ls "$TARGET_DIR" | wc -l)
+
+if [ "$FILE_COUNT" -gt 0 ]; then
+    echo "Files exist in the prefix: $TARGET_DIR (found $FILE_COUNT items)"
+    exit 0
+else
+    echo "No files found in the prefix: $TARGET_DIR -- proceeding with processing"
+fi
+
 
 # Step 0: preclean
 echo "Pre-clean local storage..."
 rm -rf "/mnt/raid0/${X}_output"
 rm -rf "/mnt/raid0/$X"
 rm -rf "/mnt/raid0/ed_working_dir"
+
 
 
 # Step 1: Copy from S3 to local storage
