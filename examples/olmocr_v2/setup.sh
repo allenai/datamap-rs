cf0f24068d05bdad21887192a680fb039ec99d79
Luca Soldaini <lucas@allenai.org>
2025-05-23 21:22:35 -0700

    Update model paths in config.yaml and setup.sh for improved data handling
    Changed the FastText model path in config.yaml to a new absolute location. In setup.sh, refactored data source and destination paths to use S3 prefixes, ensuring models are downloaded correctly and local directories are set up properly. This enhances the overall data processing workflow.

--- examples/olmocr_v2/setup.sh
+++ examples/olmocr_v2/setup.sh
@@ -8,25 +8,22 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # go to top directory with the script to download the models
 current_dir=$(pwd)
 
-# i/o locations
-# source_data="tmp/s2pdf_dedupe_minhash_v1_with_no_pii_basic_quality_30b/documents"
-# output_data="tmp/s2pdf_dedupe_minhash_v1_with_no_pii_basic_quality_30b_filtered/documents"
-source_data="tmp/s2pdf_subset"
-output_data="tmp/s2pdf_subset_filtered"
-model_name="test_pdfs_references_fasttext_model_v2_pretok_wn3_ws10_lr05_e20_short"
-# source_data="/mnt/raid0/smolpdf"
-# output_data="/mnt/raid0/smolpdf_quality_filtered"
+# download the source data
+remote_source_prefix="s3://ai2-llm/pretraining-data/sources/s2pdf_dedupe_minhash_v1_with_no_pii_basic_quality_30b/documents"
+local_source_prefix=$(echo $remote_source_prefix | sed 's|^s3://|/mnt/raid0/|g')
+local_destination_prefix=$(echo $local_source_prefix | sed 's|/documents|_no_refs|g')
 
+if [ ! -d $local_source_prefix ]; then
+    s5cmd cp -sp "${remote_source_prefix}/*" "${local_source_prefix}/"
+fi
 
+model_name="test_pdfs_references_fasttext_model_v2_pretok_wn3_ws10_lr05_e20_short.bin"
 
 # location of the models
-# models_dir="/mnt/raid0/models"
-models_dir="tmp/models"
-
-
+models_dir="/mnt/raid0/models"
 mkdir -p $models_dir
 
-# download alex fasttext fineweb-edu (only if not already present)
+# download model if not already present
 if [ ! -f "$models_dir/$model_name.bin" ]; then
     s5cmd cp -sp s3://ai2-llm/models/fasttext_reference_classifiers/$model_name.bin $models_dir/$model_name.bin
 fi
@@ -39,3 +36,6 @@ cargo run --release -- map --input-dir $source_data --output-dir $output_data --
 
 # return to the directory the script was run from
 cd $current_dir
+
+remote_destination_prefix=$(echo $local_destination_prefix | sed 's|^/mnt/raid0/|s3://|g')
+s5cmd cp -sp "${local_destination_prefix}/step_final/*" "${remote_destination_prefix}/"
