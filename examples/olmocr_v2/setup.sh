5ed5ff4b08e943af2b736c10cd2739e50bf41824
Jake Poznanski <jakep@allenai.org>
2025-05-29 23:28:13 +0000

    Running all the best set of rules so far on datadelved s2pdfs

--- examples/olmocr_v2/setup.sh
+++ examples/olmocr_v2/setup.sh
@@ -1,41 +1,74 @@
-#!/bin/bash
+# #!/bin/bash
 
-set -ex
+# set -ex
 
-# get the directory of the script
-script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# # get the directory of the script
+# script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-# go to top directory with the script to download the models
-current_dir=$(pwd)
+# # go to top directory with the script to download the models
+# current_dir=$(pwd)
 
-# download the source data
-remote_source_prefix="s3://ai2-llm/pretraining-data/sources/s2pdf_dedupe_minhash_v1_with_no_pii_basic_quality_30b/documents"
-local_source_prefix=$(echo $remote_source_prefix | sed 's|^s3://|/mnt/raid0/|g')
-local_destination_prefix=$(echo $local_source_prefix | sed 's|/documents|_no_refs|g')
+# # download the source data
+# remote_source_prefix="s3://ai2-llm/pretraining-data/sources/s2pdf_dedupe_minhash_v1_with_no_pii_basic_quality_30b/documents"
+# local_source_prefix=$(echo $remote_source_prefix | sed 's|^s3://|/mnt/raid0/|g')
+# local_destination_prefix=$(echo $local_source_prefix | sed 's|/documents|_no_refs|g')
 
-if [ ! -d $local_source_prefix ]; then
-    s5cmd cp -sp "${remote_source_prefix}/*" "${local_source_prefix}/"
-fi
+# if [ ! -d $local_source_prefix ]; then
+#     s5cmd cp -sp "${remote_source_prefix}/*" "${local_source_prefix}/"
+# fi
 
-model_name="test_pdfs_references_fasttext_model_v2_pretok_wn3_ws10_lr05_e20_short"
+# model_name="test_pdfs_references_fasttext_model_v2_pretok_wn3_ws10_lr05_e20_short"
 
-# location of the models
-models_dir="/mnt/raid0/models"
-mkdir -p $models_dir
+# # location of the models
+# models_dir="/mnt/raid0/models"
+# mkdir -p $models_dir
 
-# download model if not already present
-if [ ! -f "$models_dir/$model_name.bin" ]; then
-    s5cmd cp -sp s3://ai2-llm/models/fasttext_reference_classifiers/$model_name.bin $models_dir/$model_name.bin
-fi
+# # download model if not already present
+# if [ ! -f "$models_dir/$model_name.bin" ]; then
+#     s5cmd cp -sp s3://ai2-llm/models/fasttext_reference_classifiers/$model_name.bin $models_dir/$model_name.bin
+# fi
 
-# move to top directory with the rust code
-cd $script_dir/../../
+# # move to top directory with the rust code
+# cd $script_dir/../../
 
-# run the project
-cargo run --release -- map --input-dir $local_source_prefix --output-dir $local_destination_prefix --config $script_dir/config.yaml
+# run the project for
+CATEGORIES=(
+  adult
+  art_design
+  crime_law
+  education_jobs
+  entertainment
+  fashion_beauty
+  finance_business
+  food_dining
+  games
+  hardware
+  health
+  history
+  home_hobbies
+  industrial
+  literature
+  politics
+  religion
+  science_tech
+  social_life
+  software
+  software_dev
+  sports_fitness
+  transportation
+  travel
+)
 
-# return to the directory the script was run from
-cd $current_dir
+for CATEGORY in "${CATEGORIES[@]}"; do
+  echo "Processing $CATEGORY..."
+  cargo run --release -- map \
+    --input-dir "/mnt/raid0/s2pdf_dedupe_minhash_v1_with_no_pii_basic_quality_datadelve/${CATEGORY}/documents" \
+    --output-dir "/mnt/raid0/s2pdf_dedupe_minhash_v1_with_no_pii_basic_quality_datadelve_norefs_mdtables_v2/${CATEGORY}/" \
+    --config ./examples/olmocr_v2/config.yaml
+done
 
-remote_destination_prefix=$(echo $local_destination_prefix | sed 's|^/mnt/raid0/|s3://|g')
-s5cmd cp -sp "${local_destination_prefix}/step_final/*" "${remote_destination_prefix}/"
+# # return to the directory the script was run from
+# cd $current_dir
+
+# remote_destination_prefix=$(echo $local_destination_prefix | sed 's|^/mnt/raid0/|s3://|g')
+# s5cmd cp -sp "${local_destination_prefix}/step_final/*" "${remote_destination_prefix}/"
