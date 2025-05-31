6e1c4c1131feed637f21be410b7b2ce12b4f56e1
Matt Jordan <mattj@allenai.org>
2025-05-30 17:22:11 -0700

    added ad3 minhash stuff

---
+++ examples/all_dressed/v3_minhash/sort_download_builder.py
@@ -0,0 +1,22 @@
+import sys
+
+INCREMENT = 1024
+PRES = ["%02d" % i for i in range(32)]
+
+
+def main(idx):
+    idx = int(idx)
+    output_file = "/mnt/raid0/downloader_%02d.txt" % idx
+    with open(output_file, "w") as f:
+        for pre in PRES:
+            for chunk_id in range(idx * INCREMENT, idx * INCREMENT + INCREMENT):
+                line = (
+                    "cp s3://ai2-llm/pretraining-data/sources/cc_all_dressed/all_dressed_v3/grouped/%s/chunk_%08d.* /mnt/raid0/input/\n"
+                    % (pre, chunk_id)
+                )
+                f.write(line)
+
+
+if __name__ == "__main__":
+    idx = sys.argv[1]
+    main(idx)
---
+++ examples/all_dressed/v3_minhash/sortfilter_pmr/part_08.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+
+# Store the input argument
+X=08
+
+
+echo "Processing directory: $X"
+
+# Step -1:
+TARGET_DIR="s3://ai2-llm/pretraining-data/sources/cc_all_dressed/all_dressed_v3/minhash/param_26_11/filtered/$X/"
+FILE_COUNT=$(aws s3 ls "$TARGET_DIR" | wc -l)
+
+if [ "$FILE_COUNT" -gt 0 ]; then
+    echo "Files exist in the prefix: $TARGET_DIR (found $FILE_COUNT items)"
+    exit 0
+else
+    echo "No files found in the prefix: $TARGET_DIR -- proceeding with processing"
+fi
+
+
+
+
+
+
+# Step 0: preclean
+echo "Pre-clean local storage..."
+rm -rf "/mnt/raid0/input"
+rm -rf "/mnt/raid0/sorted"
+rm -rf "/mnt/raid0/output"
+
+
+
+
+# Step 1: Copy from S3 to local storage
+echo "Copying data from S3 to local storage..."
+cd ~/datamap-rs
+git checkout sort_v2; git pull
+python3 examples/all_dressed/v3_minhash/sort_download_builder.py $X
+s5cmd run "/mnt/raid0/downloader_${X}.txt"
+
+
+
+# Step 2: Run the map operation
+echo "Running sort/filter operations..."
+
+cargo run --release -- dist-sort --input-dir /mnt/raid0/input --output-dir /mnt/raid0/sorted  --config examples/all_dressed/minhash_groupsort.yaml  > "/mnt/raid0/sort_${X}.log"
+cargo run --release -- group-sort-filter --input-dir /mnt/raid0/sorted --output-dir /mnt/raid0/output  --config examples/all_dressed/minhash_groupsort.yaml  > "/mnt/raid0/filtered_${X}.log"
+
+
+
+echo "Copying results back to S3..."
+s5cmd cp -sp /mnt/raid0/sorted/ "s3://ai2-llm/pretraining-data/sources/cc_all_dressed/all_dressed_v3/minhash_param_26_11/sorted/${X}/"
+s5cmd cp -sp /mnt/raid0/output/ "s3://ai2-llm/pretraining-data/sources/cc_all_dressed/all_dressed_v3/minhash/param_26_11/filtered/${X}/"
+s5cmd cp -sp "/mnt/raid0/*.log" s3://ai2-llm/pretraining-data/sources/cc_all_dressed/all_dressed_v3/logs/
+
+echo "Processing complete for $X"
