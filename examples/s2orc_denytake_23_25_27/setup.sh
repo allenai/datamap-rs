358b171133656c0ac5ee17cdb2dbd4418adaf7a5
Jake Poznanski <jakep@allenai.org>
2025-06-04 20:20:40 +0000

    Saving some filtering configs for reference

---
+++ examples/s2orc_denytake_23_25_27/setup.sh
@@ -0,0 +1,36 @@
+
+CATEGORIES=(
+  adult
+  art_design
+  crime_law
+  education_jobs
+  entertainment
+  fashion_beauty
+  finance_business
+  food_dining
+  games
+  hardware
+  health
+  history
+  home_hobbies
+  industrial
+  literature
+  politics
+  religion
+  science_tech
+  social_life
+  software
+  software_dev
+  sports_fitness
+  transportation
+  travel
+)
+
+for CATEGORY in "${CATEGORIES[@]}"; do
+  echo "Processing $CATEGORY..."
+  cargo run --release -- map \
+    --input-dir "/mnt/raid0/s2pdf_dedupe_minhash_v1_with_no_pii_basic_quality_datadelve_norefs_mdtables_v2/${CATEGORY}/step_final" \
+    --output-dir "/mnt/raid0/s2pdf_dedupe_minhash_v1_with_no_pii_basic_quality_datadelve_norefs_mdtables_v2_denylisted/${CATEGORY}/" \
+    --config ./examples/olmocr_intake9_10_11/config.yaml
+done
+
