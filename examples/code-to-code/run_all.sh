1c3fb865cf9d1f6804c215fa534c4bda47fc6cd0
Luca Soldaini <luca@soldaini.net>
2025-06-05 20:26:56 +0000

    Update base.yaml to use a placeholder for fasttext file and enhance run_all.sh with instance rank and world size logic for processing languages in parallel.

--- examples/code-to-code/run_all.sh
+++ examples/code-to-code/run_all.sh
@@ -15,27 +15,74 @@ languages=(
     "TypeScript"
 )
 
+remote_dir="s3://ai2-llm/pretraining-data/sources/the-stack-v2/spring2code_v2/minhash_v2"
+remote_dest_dir="s3://ai2-llm/pretraining-data/sources/the-stack-v2/spring2code_v2/minhash_v2_annotated"
+fasttext_remote_dir="s3://ai2-llm/fasttext_models/code-vs-code"
+
 source_dir="${HOME}/ai2-llm/pretraining-data/sources/the-stack-v2/spring2code_v2/minhash_v2/data"
-output_dir="${HOME}/ai2-llm/pretraining-data/sources/the-stack-v2/spring2code_v2/minhash_v2_annotated"
-error_dir="${HOME}/ai2-llm/pretraining-data/sources/the-stack-v2/spring2code_v2/minhash_v2_annotated_error"
+output_dir="${HOME}/ai2-llm/pretraining-data/sources/the-stack-v2/spring2code_v2/minhash_v2_annotated/data"
+error_dir="${HOME}/ai2-llm/pretraining-data/sources/the-stack-v2/spring2code_v2/minhash_v2_annotated/error"
+fasttext_dir="${HOME}/ai2-llm/fasttext_resources/code-vs-code/model"
 
-for language in "${languages[@]}"; do
-    if [ "$language" == "C++" ]; then
-        model_name="whitespace-Cpp_lr05_ng3_n3M6"
-    elif [ "$language" == "C-Sharp" ]; then
-        model_name="whitespace-CSharp_lr05_ng3_n3M6"
+get_instance_rank () {
+    instance_id=$(ec2-metadata --instance-id | cut -d ' ' -f 2)
+    instance_name=$(aws ec2 describe-instances --instance-ids $instance_id --query 'Reservations[0].Instances[0].Tags[?Key==`Name`].Value' --output text)
+    instance_rank=$(echo $instance_name | cut -d '-' -f 3)
+    echo $instance_rank
+}
+
+get_world_size () {
+    instance_id=$(ec2-metadata --instance-id | cut -d ' ' -f 2)
+    project_name=$(aws ec2 describe-instances --instance-ids $instance_id --query 'Reservations[0].Instances[0].Tags[?Key==`Project`].Value' --output text)
+    world_size=$(aws ec2 describe-instances \
+        --filters "Name=tag:Project,Values=${project_name}" "Name=instance-state-name,Values=running" \
+        --query 'length(Reservations[*].Instances[*])' \
+        --output text)
+    echo $world_size
+}
+
+mkdir -p $fasttext_dir
+
+
+# Get instance rank and world size
+instance_rank=$(get_instance_rank)
+world_size=$(get_world_size)
+
+echo "Instance rank: $instance_rank, World size: $world_size"
+
+# Process languages based on rank
+for i in "${!languages[@]}"; do
+    # Check if this language index should be processed by this instance
+    if [ $((i % world_size)) -eq $instance_rank ]; then
+        language="${languages[$i]}"
+
+        if [ "$language" == "C++" ]; then
+            model_name="whitespace-Cpp_lr05_ng3_n3M6.bin"
+        elif [ "$language" == "C-Sharp" ]; then
+            model_name="whitespace-CSharp_lr05_ng3_n3M6.bin"
+        else
+            model_name="whitespace-${language}_lr05_ng3_n3M6.bin"
+        fi
+
+        fasttext_local_path="${fasttext_dir}/${model_name}"
+        s5cmd cp -sp "${fasttext_remote_dir}/${model_name}" "${fasttext_local_path}"
+
+        cat examples/code-to-code/base.yaml | sed "s/FASTTEXT_FILE/${fasttext_local_path}/g" > examples/code-to-code/${language}.yaml
+
+        s5cmd cp -sp "${remote_dir}/${language}/*" "${source_dir}/${language}/"
+
+        echo "Running $language (index $i)"
+        mkdir -p $output_dir/$language
+        mkdir -p $error_dir/$language
+        cargo run --release -- map \
+            --config examples/code-to-code/$language.yaml \
+            --input-dir $source_dir/$language \
+            --output-dir $output_dir/$language \
+            --err-dir $error_dir/$language
+
+        s5cmd cp -sp "${output_dir}/${language}/*" "${remote_dest_dir}/${language}/"
+        s5cmd cp -sp "${error_dir}/${language}/*" "${remote_dest_dir}/${language}/error/"
     else
-        model_name="whitespace-${language}_lr05_ng3_n3M6"
+        echo "Skipping ${languages[$i]} (index $i) - assigned to rank $((i % world_size))"
     fi
-
-    cat examples/code-to-code/base.yaml | sed "s/MODEL_NAME_REPLACE_ME/${model_name}/g" > examples/code-to-code/${language}.yaml
-
-    echo "Running $language"
-    mkdir -p $output_dir/$language
-    mkdir -p $error_dir/$language
-    cargo run --release -- map \
-        --config examples/code-to-code/$language.yaml \
-        --input-dir $source_dir/$language \
-        --output-dir $output_dir/$language \
-        --err-dir $error_dir/$language
 done
