8e24181d79fbd6227a95bdb04a28f8538ca9c64a
Luca Soldaini <luca@soldaini.net>
2025-06-05 21:16:04 +0000

    Update run_all.sh to change remote directory structure for output and error files

--- examples/code-to-code/run_all.sh
+++ examples/code-to-code/run_all.sh
@@ -21,7 +21,7 @@ languages=(
 
 DRIVE="${DRIVE:-/mnt/raid0}"
 
-remote_dir="s3://ai2-llm/pretraining-data/sources/the-stack-v2/spring2code_v2/minhash_v2"
+remote_dir="s3://ai2-llm/pretraining-data/sources/the-stack-v2/spring2code_v2/minhash_v2/data"
 remote_dest_dir="s3://ai2-llm/pretraining-data/sources/the-stack-v2/spring2code_v2/minhash_v2_annotated"
 fasttext_remote_dir="s3://ai2-llm/fasttext_models/code-vs-code"
 
@@ -93,8 +93,8 @@ for i in "${!languages[@]}"; do
             --output-dir $output_dir/$language \
             --err-dir $error_dir/$language
 
-        s5cmd cp -sp "${output_dir}/${language}/*" "${remote_dest_dir}/${language}/"
-        s5cmd cp -sp "${error_dir}/${language}/*" "${remote_dest_dir}/${language}/error/"
+        s5cmd cp -sp "${output_dir}/${language}/*" "${remote_dest_dir}/data/${language}"
+        s5cmd cp -sp "${error_dir}/${language}/*" "${remote_dest_dir}/error/${language}"
     else
         echo "Skipping ${languages[$i]} (index $i) - assigned to rank $((i % world_size))"
     fi
