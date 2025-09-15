486ef92dd4e58d7ec58cb7e1dea52f190d29ebfa
Jake Poznanski <jakep@allenai.org>
2025-09-15 16:08:46 +0000

    Configs for second filtering out step

--- examples/s2pdf_denytake_48_51/setup.sh
+++ examples/s2pdf_denytake_48_51/setup.sh
@@ -29,8 +29,8 @@ CATEGORIES=(
 for CATEGORY in "${CATEGORIES[@]}"; do
   echo "Processing $CATEGORY..."
   cargo run --release -- map \
-    --input-dir "/mnt/raid0/s2pdf_dedupe_minhash_v1_with_no_pii_basic_quality_datadelve_norefs_mdtables_v2/${CATEGORY}/step_final" \
-    --output-dir "/mnt/raid0/s2pdf_dedupe_minhash_v1_with_no_pii_basic_quality_datadelve_norefs_mdtables_v2_denylisted/${CATEGORY}/" \
-    --config ./examples/s2orc_denytake_48_51/config.yaml
+    --input-dir "/mnt/raid0/s2pdf_dedupe_minhash_v1_with_no_pii_basic_quality_datadelve_norefs_mdtables_v2_denylisted_reshard/${CATEGORY}" \
+    --output-dir "/mnt/raid0/s2pdf_dedupe_minhash_v1_with_no_pii_basic_quality_datadelve_norefs_mdtables_v2_denylisted_reshard_denymore/${CATEGORY}/" \
+    --config ./examples/s2pdf_denytake_48_51/config.yaml
 done
 
