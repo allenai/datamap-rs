1dec8bd880973a34232efccb072efef60c5d6a24
Luca Soldaini <lucas@allenai.org>
2026-01-17 21:33:39 -0800

    lowercasing

--- configs/code/filter_languages.sh
+++ configs/code/filter_languages.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-INPUT_DIR="/mnt/raid0/ai2-llm/pretraining-data/sources/the-stack-v2/spring2code_v2/minhash_v2_annotated_reshard_qc_tagged/"
-OUTPUT_DIR="/mnt/raid0/ai2-llm/pretraining-data/sources/the-stack-v2/spring2code_v2/minhash_v2_annotated_reshard_qc_tagged_filtered/"
+INPUT_DIR="/mnt/raid0/ai2-llm/pretraining-data/sources/the-stack-v2/spring2code_v2/minhash_v2_annotated_reshard_qc_tagged"
+OUTPUT_DIR="/mnt/raid0/ai2-llm/pretraining-data/sources/the-stack-v2/spring2code_v2/minhash_v2_annotated_reshard_qc_tagged_filtered"
 
 
 for language in $(ls --color=never ${INPUT_DIR}); do
@@ -9,5 +9,5 @@ for language in $(ls --color=never ${INPUT_DIR}); do
     cargo run --release map \
         --input-dir ${INPUT_DIR}/${language}/step_final/ \
         --output-dir ${OUTPUT_DIR}/${language}/ \
-        --config configs/code/filters/${language}.yaml
+        --config "configs/code/filters/$(echo ${language} | tr '[:upper:]' '[:lower:]').yaml"
 done
