abeed1765466ceccea5dffbd19b01ae4bedb4475
Luca Soldaini <lucas@allenai.org>
2026-01-17 21:51:40 -0800

    tokenization script

---
+++ configs/all_dressed/tokenize_language.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+set -ex
+
+BASE_DIR="/mnt/raid0"
+TOKENIZER_NAME="allenai/dolma2-tokenizer"
+INPUT_DIR="${BASE_DIR}/pretraining-data/sources/the-stack-v2/spring2code_v2/minhash_v2_annotated_reshard_qc_tagged_filtered"
+OUTPUT_DIR="${BASE_DIR}/preprocessed/the-stack-v2/spring2code_v2/minhash_v2_annotated_reshard_qc_tagged_filtered"
+PROGRAMMING_LANGUAGE=$1
+
+if [ -z "${PROGRAMMING_LANGUAGE}" ]; then
+    echo "Programming language is required"
+    exit 1
+fi
+
+if [ ! -d ".venv" ]; then
+    # setting up virtual environment
+    uv venv
+fi
+
+# installing dolma
+uv pip install dolma
+
+# downloading dolma2-tokenizer
+uv run --with=huggingface-hub \
+    hf download ${TOKENIZER_NAME} \
+    --local-dir ${BASE_DIR}/huggingface/${TOKENIZER_NAME}
+
+# run dolma once to make sure ntlk is downloaded
+uv run dolma
+
+# tokenizing the language
+uv run dolma tokens \
+    --documents "${INPUT_DIR}/${PROGRAMMING_LANGUAGE}/step_final/*" \
+    --destination "${OUTPUT_DIR}/${PROGRAMMING_LANGUAGE}/${TOKENIZER_NAME}" \
+    --tokenizer.name_or_path ${TOKENIZER_NAME} \
+    --tokenizer.eos_token_id 100257 \
+    --tokenizer.pad_token_id 100277 \
+    --no-tokenizer.segment_before_tokenization \
+    --tokenizer.encode_special_tokens \
+    --processes $(python3 -c "import multiprocessing; print(multiprocessing.cpu_count())") \
+    --max_size 4_000_000_000 \
+    --sample_ring_prop \
+    --dtype uint32
