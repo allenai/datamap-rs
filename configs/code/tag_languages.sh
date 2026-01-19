794d39d3e23b687f199aceca38407fcf3b5fb229
Luca Soldaini <lucas@allenai.org>
2026-01-19 14:28:29 -0800

    Refactor tagging and sampling script to use a predefined array of languages, improving readability and maintainability. Added checks for existing output directories and files to prevent overwriting.

--- configs/code/tag_languages.sh
+++ configs/code/tag_languages.sh
@@ -5,22 +5,57 @@ INPUT_DIR="${BASE_DIR}/ai2-llm/pretraining-data/sources/the-stack-v2/spring2code
 OUTPUT_DIR="${BASE_DIR}/ai2-llm/pretraining-data/sources/the-stack-v2/spring2code_v2/minhash_v2_annotated_reshard_qc_tagged"
 
 
-for upper_lang in $(ls --color=never ${INPUT_DIR}); do
-    lower_lang=$(echo ${upper_lang} | tr '[:upper:]' '[:lower:]')
-    echo "Tagging ${upper_lang}..."
-    cargo run --release map \
-        --input-dir ${INPUT_DIR}/${upper_lang}/step_final/ \
-        --output-dir ${OUTPUT_DIR}/${upper_lang}/ \
-        --config configs/code/classifiers/${lower_lang}.yaml
+LANGUAGES=(
+    "C"
+    "C++"
+    "C-Sharp"
+    "Go"
+    "Java"
+    "JavaScript"
+    "Markdown"
+    "PHP"
+    "Python"
+    "Ruby"
+    "Rust"
+    "Shell"
+    "SQL"
+    "Swift"
+    "TypeScript"
+)
+
+
+for language in "${LANGUAGES[@]}"; do
+    if [ "${language}" == "C++" ]; then
+        config_file="configs/code/classifiers/cpp.yaml"
+    elif [ "${language}" == "C-Sharp" ]; then
+        config_file="configs/code/classifiers/csharp.yaml"
+    else
+        config_file="configs/code/classifiers/$(echo ${language} | tr '[:upper:]' '[:lower:]').yaml"
+    fi
+
+    input_dir="${INPUT_DIR}/${language}/step_final/"
+    output_dir="${OUTPUT_DIR}/${language}/"
+
+    if [-d "${output_dir}"]; then
+        echo "Output directory ${output_dir} already exists"
+        continue
+    fi
+
+    echo "Tagging ${language} with config ${config_file}..."
+    cargo run --release map --input-dir ${input_dir} --output-dir ${output_dir} --config ${config_file}
 done
 
 
-for upper_lang in $(ls --color=never ${OUTPUT_DIR}); do
-    echo "Sampling ${upper_lang}..."
-    cargo run --release reservoir-sample \
-    --input-dir ${OUTPUT_DIR}/${upper_lang}/step_final/ \
-    --output-file ${OUTPUT_DIR}/${upper_lang}/code_quality_report.json \
-    --key "metadata.code_quality.__label__pos" \
-    --text-key text \
-    --token-weighted
+for language in "${LANGUAGES[@]}"; do
+    input_dir="${OUTPUT_DIR}/${language}/step_final/"
+    output_file="${OUTPUT_DIR}/${language}/code_quality_report.json"
+
+    if [ -f "${output_file}" ]; then
+        echo "Output file ${output_file} already exists"
+        continue
+    fi
+
+    echo "Sampling ${language}..."
+    cargo run --release reservoir-sample --input-dir ${input_dir} --output-file ${output_file} \
+    --key "metadata.code_quality.__label__pos" --text-key text --token-weighted --reservoir-size 100000
 done
