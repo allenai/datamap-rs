ceca5c017e1d9cb154cc0273aba30a1dcd008304
EC2 Default User <ec2-user@ip-172-31-11-106.ec2.internal>
2026-01-20 23:57:51 +0000

    Update filter script with safer shell options

--- configs/code/filter_languages.sh
+++ configs/code/filter_languages.sh
@@ -4,6 +4,9 @@ INPUT_DIR="/mnt/raid0/ai2-llm/pretraining-data/sources/the-stack-v2/spring2code_
 OUTPUT_DIR="/mnt/raid0/ai2-llm/pretraining-data/sources/the-stack-v2/spring2code_v2/minhash_v2_annotated_reshard_qc_tagged_filtered_fixed"
 
 
+set -exou
+
+
 for language in $(ls --color=never ${INPUT_DIR}); do
     echo "Filtering ${language}..."
     cargo run --release map \
