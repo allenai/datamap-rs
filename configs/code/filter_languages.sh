550e9e0d0f4903df85772b12a1d281bffd0b5aec
Luca Soldaini <luca@soldaini.net>
2026-01-21 20:50:02 -0800

    Update language filtering script with flexible paths

--- configs/code/filter_languages.sh
+++ configs/code/filter_languages.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
-INPUT_DIR="/mnt/raid0/ai2-llm/pretraining-data/sources/the-stack-v2/spring2code_v2/minhash_v2_annotated_reshard_qc_tagged_fixed"
-OUTPUT_DIR="/mnt/raid0/ai2-llm/pretraining-data/sources/the-stack-v2/spring2code_v2/minhash_v2_annotated_reshard_qc_tagged_filtered_fixed"
+set -euox pipefail
 
-
-set -exou
+REMOTE_DIR="s3://ai2-llm"
+LOCAL_DIR="/mnt/raid0/ai2-llm"
+INPUT_DIR="pretraining-data/sources/the-stack-v2/spring2code_v2/minhash_v2_annotated_reshard_qc_tagged_auto5"
+OUTPUT_DIR="pretraining-data/sources/the-stack-v2/spring2code_v2/minhash_v2_annotated_reshard_qc_tagged_auto5_filtered"
 
 
 for language in $(ls --color=never ${INPUT_DIR}); do
     echo "Filtering ${language}..."
     cargo run --release map \
-        --input-dir ${INPUT_DIR}/${language}/step_final/ \
-        --output-dir ${OUTPUT_DIR}/${language}/ \
-        --config "configs/code/filters/$(echo ${language} | tr '[:upper:]' '[:lower:]').yaml"
+        --input-dir "${LOCAL_DIR}/${INPUT_DIR}/${language}/step_final/" \
+        --output-dir "${LOCAL_DIR}/${OUTPUT_DIR}/${language}/" \
+        --config "${CONFIGS_DIR}/$(echo ${language} | tr '[:upper:]' '[:lower:]').yaml"
 done
