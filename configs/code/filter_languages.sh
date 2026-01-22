44e83732250021f219013fd6ff5b34396e2eb117
Luca Soldaini <luca@soldaini.net>
2026-01-21 20:51:13 -0800

    configs/code/filter_languages.sh: line 13: CONFIGS_DIR: unbound variable

--- configs/code/filter_languages.sh
+++ configs/code/filter_languages.sh
@@ -6,6 +6,7 @@ REMOTE_DIR="s3://ai2-llm"
 LOCAL_DIR="/mnt/raid0/ai2-llm"
 INPUT_DIR="pretraining-data/sources/the-stack-v2/spring2code_v2/minhash_v2_annotated_reshard_qc_tagged_auto5"
 OUTPUT_DIR="pretraining-data/sources/the-stack-v2/spring2code_v2/minhash_v2_annotated_reshard_qc_tagged_auto5_filtered"
+CONFIGS_DIR="configs/code/flt_auto5"
 
 
 for language in $(ls --color=never "${LOCAL_DIR}/${INPUT_DIR}"); do
