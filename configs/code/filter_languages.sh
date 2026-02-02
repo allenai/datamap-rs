0f25e8c1717fe4e2d4d8e67935099fbfbabf6f8d
Luca Soldaini <luca@soldaini.net>
2026-02-02 08:09:25 -0800

    Add p0 float_filter with upper_bound to all code filter configs

--- configs/code/filter_languages.sh
+++ configs/code/filter_languages.sh
@@ -98,22 +98,21 @@ for language in "${LANGUAGES[@]}"; do
         continue
     fi
 
+    # download files
+    if [ ! -d "${local_input_dir}" ]; then
+        remote_input_dir="${REMOTE_DIR}/${INPUT_DIR}/${language}"
+        s5cmd cp -sp "${remote_input_dir}/*" "${local_input_dir}/"
+    fi
+
     # unltil there's a directory called "step_final", replace local_output_dir with step final;
     # otherwise break out of the loop.
     while true; do
-        if [ -d "${local_output_dir}/step_final" ]; then
-            local_output_dir="${local_output_dir}/step_final"
+        if [ -d "${local_input_dir}/step_final" ]; then
+            local_input_dir="${local_input_dir}/step_final"
         else
             break
         fi
     done
-
-
-    if [ ! -d "${local_input_dir}" ]; then
-        remote_input_dir="${REMOTE_DIR}/${INPUT_DIR}/${language}"
-
-        s5cmd cp -sp "${remote_input_dir}/*" "${local_input_dir}/"
-    fi
 
     echo "Filtering ${language} with config ${config_file}..."
     cargo run --release map \
